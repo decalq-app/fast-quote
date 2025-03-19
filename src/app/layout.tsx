b6d2fe126ffe698203727397e8181b294099adef
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2025-03-18 22:16:08 -0300

    feat: Add Textarea and Select components, update layout to include Toaster, and enhance package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Toaster } from '@/components/ui/sonner'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Decalq",
-  description: "Decalq",
+  title: 'Decalq',
+  description: 'Decalq'
 }
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   )
 }
--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -18,9 +18,60 @@ import {
   DrawerTitle,
   DrawerTrigger
 } from '@/components/ui/drawer'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import type { Link } from 'lucide-react'
+import { Textarea } from '@/components/ui/textarea'
+import { toast } from 'sonner'
+import { useState } from 'react'
+
+const formSchema = z.object({
+  references: z.custom<File>(value => value instanceof File, {
+    message: 'Selecione um arquivo válido'
+  }),
+  partOfBody: z.string().min(1, 'Este campo é obrigatório'),
+  size: z.coerce.number().min(0, 'Este campo é obrigatório'),
+  type: z.enum(['normal', 'cobertura', 'retoque', 'reforma']),
+  idea: z.string().min(1, 'Este campo é obrigatório')
+})
 
 export default function InkerProfile () {
-  const router = useRouter()
+  const router = useRouter();
+  const [drawer, setDrawer] = useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      partOfBody: '',
+      size: 0,
+      type: 'normal',
+      idea: ''
+    }
+  })
+
+  function onSubmit (values: z.infer<typeof formSchema>) {
+    toast("Orçamento enviado com sucesso! Aguarde o contato do artista.")
+    setDrawer(false)
+    console.log(values)
+  }
 
   const handleLogOut = () => {
     signOut(auth)
@@ -36,26 +87,131 @@ export default function InkerProfile () {
         <ProfileDetails />
 
         <div className='flex flex-col my-4 gap-y-2'>
-          <Drawer>
-            <DrawerTrigger>
-              <Button className='w-full'>Orçamento rápido</Button>
+          <Drawer open={drawer} onClose={() => setDrawer(false)}>
+            <DrawerTrigger asChild>
+              <Button className='w-full' onClick={() => setDrawer(true)}>Orçamento rápido</Button>
             </DrawerTrigger>
             <DrawerContent>
-              <DrawerHeader>
-                <DrawerTitle>Orçamento rápido</DrawerTitle>
-                <DrawerDescription>
-                  Envie referencias e detalhes do seu projeto para que possamos
-                  te ajudar.
-                </DrawerDescription>
-              </DrawerHeader>
-              <DrawerFooter>
-                <Button>Submit</Button>
-                <DrawerClose>
-                  <Button className='w-full' variant='outline'>
-                    Cancel
-                  </Button>
-                </DrawerClose>
-              </DrawerFooter>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-8'
+              >
+                <DrawerHeader>
+                  <DrawerTitle>Orçamento rápido</DrawerTitle>
+                  <DrawerDescription>
+                    Envie referencias e detalhes do seu projeto para que
+                    possamos te ajudar.
+                  </DrawerDescription>
+                </DrawerHeader>
+                <div className='px-4 flex flex-col gap-y-4'>
+                  <Form {...form}>
+                    <FormField
+                      control={form.control}
+                      name='references'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Uma imagem de referencia</FormLabel>
+                          <FormControl>
+                            {/* // TODO: Limitar tamanho e quantidade de arquivos */}
+                            <Input
+                              type='file'
+                              accept='.jpg, .jpeg, .png, .svg'
+                              onChange={e =>
+                                field.onChange(
+                                  e.target.files ? e.target.files[0] : null
+                                )
+                              }
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='partOfBody'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Parte do corpo</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='size'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>
+                            Tamanho da tatuagem (em centímetros)
+                          </FormLabel>
+                          <FormControl>
+                            <Input type='number' {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name='type'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Tipo da tatuagem</FormLabel>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <FormControl>
+                              <SelectTrigger className='w-full'>
+                                <SelectValue placeholder='Tipo da tatuagem' />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <SelectItem value='normal'>Normal</SelectItem>
+                              <SelectItem value='cobertura'>
+                                Cobertura
+                              </SelectItem>
+                              <SelectItem value='retoque'>Retoque</SelectItem>
+                              <SelectItem value='reforma'>Reforma</SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name='idea'
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Descreva sua ideia</FormLabel>
+                          <FormControl>
+                            <Textarea
+                              placeholder='Conte mais sobre sua ideia'
+                              className='resize-none'
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </Form>
+                </div>
+                <DrawerFooter>
+                  <Button type='submit'>Submit</Button>
+                  <DrawerClose asChild>
+                    <Button className='w-full' variant='outline'>
+                      Cancel
+                    </Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </form>
             </DrawerContent>
           </Drawer>
         </div>
